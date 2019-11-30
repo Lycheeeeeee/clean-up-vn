873b9ebd50f019d0424f78b1b4d7e4d9af5722a8
Apple <apple@appurunombp.stusgs.rmit.edu.vn>
2019-11-30 14:57:19 +0700

    fix cors

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
 	"github.com/Lycheeeeeee/clean-up-vn/app"
 	"github.com/Lycheeeeeee/clean-up-vn/controllers"
 	"github.com/gorilla/mux"
+	// "github.com/gorilla/handlers"
+	"github.com/rs/cors"
 )
 
 func main() {
 
 	router := mux.NewRouter()
-	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+	router.Use(app.JwtAuthentication)
+	// cors := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"*"},
+	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	//   })
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		Debug: true,
+	})
+	handler := c.Handler(router)
+	//   router.Use(cors.Handler
+	//attach JWT auth middleware
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 	router.HandleFunc("/api/user/sociallogin/{id}", controllers.Socialauthenticate).Methods("GET")
@@ -27,15 +44,15 @@ func main() {
 	router.HandleFunc("/api/userproject/adduser", controllers.CreateUserProject).Methods("POST")
 	router.HandleFunc("/api/userproject/leaveproject", controllers.DeleteUserProject).Methods("POST")
 	router.HandleFunc("/api/project/downloadvolunteerlist/{userid}", controllers.DownloadFile).Methods("POST")
-	router.HandleFunc("/api/project/testing", controllers.Testing).Methods("GET")
+	// router.HandleFunc("/api/project/testing", controllers.Testing).Methods("GET") 	
 	//port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
 	//if port == "" {
 	//port = 8000 //localhost
 	//}
 
 	fmt.Println("8000")
-
-	err := http.ListenAndServe(":8000", router) //Launch the app, visit localhost:8000/api
+	// _ = cors.Default().Handler(router)
+	err := http.ListenAndServe(":8000", handler) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		fmt.Print(err)
 	}
